6b2aee26cf5faa4fd09a43c80f2abe56ba4731ca
GOWTHAM SAI CHIRIKI <gowthamsai167@gmail.com>
2025-09-26 12:42:02 +0530

    Implemented code for StudentDAO

--- src/main/java/com/example/registration/dao/StudentDAO.java
+++ src/main/java/com/example/registration/dao/StudentDAO.java
@@ -1,4 +1,33 @@
 package com.example.registration.dao;
 
-public class StudentDAO {
+import com.example.registration.entity.Student;
+
+import java.util.List;
+
+public class StudentDAO extends BaseDAO<Student>{
+    //To save (persist) student
+    public void save(Student student){
+        executeVoid(session -> session.persist(student));
+    }
+
+    //To update the student
+    public void update(Student student){
+        executeVoid(session -> session.merge(student));
+    }
+
+    //To delete the student
+    public void delete(Student student){
+        executeVoid(session -> session.remove(session));
+    }
+
+    //To find the student by id
+    public Student findById(Long id){
+        return execute(session -> session.get(Student.class, id));
+    }
+
+    //To find the list of all student
+    public List<Student> findAll(){
+        return execute(session -> session.createQuery("FROM Student", Student.class).list());
+
+    }
 }
